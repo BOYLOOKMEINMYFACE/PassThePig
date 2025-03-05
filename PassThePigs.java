28604768e210ac16b31c1edb832d6bc360eab5b7
BOYLOOKMEINMYFACE <166769825+BOYLOOKMEINMYFACE@users.noreply.github.com>
2025-03-05 20:12:00 +0000

    some console

--- PassThePigs.java
+++ PassThePigs.java
@@ -11,27 +11,32 @@ public class PassThePigs {
     static int[] handScore;
     static Scanner sc = new Scanner(System.in);
     static boolean pigOut = false;
+    static int num;
 
     public static void main(String args[]) {
 
-        // initialize players 
+        // initialize players and Game
+        System.out.println("Welcome to PassThePigs! Are YOU ready to pass pigs?");
         System.out.print("Enter the Number of Bots: ");
-        int num = sc.nextInt() + 1;
+        num = sc.nextInt() + 1;
         sc.nextLine();
+        handScore = new int[num];
         Player jacob = new Human("Jacob");
         for (int i = 0; i < num; i++) {
             players.add((i == 0) ? jacob : new JBot("Player" + (i + 1), "NO BRAIN"));
             score.add(0);
         }
 
-        handScore = new int[num];
-
+        // Play Game
         boolean end = false;
         while (!end) {
             for (int i = 0; i < num; i++) {
                 if (inGame(i) && players.get(i) instanceof JBot) {
+                    System.out.println("________________");
+                    System.out.println(players.get(i).getName() + "'s turn!");
                     botTurn(i);
                 } else if (inGame(i)) {
+                    System.out.println("Your turn! Let's pass the pig!");
                     humanTurn(i);
                 } else {
                     end = true;
@@ -44,21 +49,24 @@ public class PassThePigs {
     }
 
     public static void botTurn(int i) {
-        System.out.print("BOT");
         while (!pigOut && inGame(i) && decidesToRoll(i)) {
+            System.out.println(players.get(i).getName() + " rolls!");
             rollPig(i);
+            displayStat();
             pigOut = (handScore[i] == 0);
         }
     }
 
     public static void humanTurn(int i) {
         while (!pigOut && inGame(i)) {
+            displayStat();
             System.out.print("Do You Want To Roll? (y/n)");
             String ans = sc.nextLine();
             if (ans.equals("y")) {
-                System.out.println("Rolled");
+                System.out.println("ROLL EM!");
                 rollPig(i);
                 pigOut = (handScore[i] == 0);
+                System.out.println("Your handScore: " + handScore[i]);
             } else {
                 break;
             }
@@ -82,6 +90,13 @@ public class PassThePigs {
         } else {
             score.set(i, score.get(i) + handScore[i]);
         }
-        System.out.println(score.get(i));
+    }
+
+    public static void displayStat() {
+        System.out.println("________________");
+        for (int i = 0; i < num; i++) {
+            System.out.println(players.get(i).getName() + " Score: " + score.get(i));
+        }
+        System.out.println("________________");
     }
 }
